bf85088cc422513112b9552c271faf1d2ba4d86d
Alejandro Chiarilli <125934002+AleChiarilli@users.noreply.github.com>
2023-07-21 13:29:27 +0000

    datastructures done

--- src/datastructures.py
+++ src/datastructures.py
@@ -9,12 +9,32 @@ update this file to implement the following already declared methods:
 
 from random import randint
 
+
 class FamilyStructure:
     def __init__(self, last_name):
         self.last_name = last_name
 
         # example list of members
-        self._members = []
+        self._members = [
+            {
+                "first_name": "John",
+                "last_name": "Jackson",
+                "age": 33,
+                "lucky_numbers": [7, 13, 22]
+            },
+            {
+                "first_name": "Jane",
+                "last_name": "Jackson",
+                "age": 35,
+                "lucky_numbers": [10, 14, 3]
+            },
+            {
+                "first_name": "Jimmy",
+                "last_name": "Jackson",
+                "age": 5,
+                "lucky_numbers": [1]
+            }
+        ]
 
     # read-only: Use this method to generate random members ID's when adding members into the list
     def _generateId(self):
@@ -23,20 +43,21 @@ class FamilyStructure:
     def add_member(self, member):
         # fill this method and update the return
         member["last_name"] = self.last_name
-        member["id"] = self._generateId()
+        if not member["id"]:
+            member["id"] = self._generateId()
         self._members.append(member)
 
     def delete_member(self, id):
         # fill this method and update the return
         for member in self._members:
-            if member["id"] == id:
+            if "id" in member and member["id"] == id:
                 self._members.remove(member)
                 break
 
     def get_member(self, id):
         # fill this method and update the return
         for member in self._members:
-            if member['id'] == id:
+            if "id" in member and member["id"] == id:
                 return member
 
     # this method is done, it returns a list with all the family members
